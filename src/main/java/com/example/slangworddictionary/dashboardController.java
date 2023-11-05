603fa13540758b4493b6221a16a4dc21bf804e38
Minh Khoi <nminhkhoi0818@gmail.com>
2023-11-05 20:07:29 +0700

    feat(dictionary): add searching by definition

--- src/main/java/com/example/slangworddictionary/dashboardController.java
+++ src/main/java/com/example/slangworddictionary/dashboardController.java
@@ -1,7 +1,9 @@
 package com.example.slangworddictionary;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -9,6 +11,7 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.util.Set;
@@ -60,7 +63,22 @@ public class dashboardController {
     private TableColumn<SlangDefinition, String> view_slang_col;
 
     @FXML
-    private TableColumn<SlangDefinition, String> view_stt_col;
+    private TableColumn<SlangDefinition, String> view_index_col;
+
+    @FXML
+    private TableView<SlangDefinition> filter_def_table;
+
+    @FXML
+    private TableColumn<SlangDefinition, String> filter_def_col;
+
+    @FXML
+    private TableColumn<SlangDefinition, String> filter_index_col;
+
+    @FXML
+    private TableColumn<SlangDefinition, String> filter_slang_col;
+
+    @FXML
+    private TextField filter_def;
 
     public void initialize() {
         Dictionary dictionary = new Dictionary();
@@ -74,11 +92,15 @@ public class dashboardController {
                     slangData.add(new SlangDefinition(slang, definition));
                 }
             }
-
+            view_index_col.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(view_table.getItems().indexOf(param.getValue()) + 1).asString());
             view_slang_col.setCellValueFactory(new PropertyValueFactory<SlangDefinition, String>("slang"));
             view_def_col.setCellValueFactory(new PropertyValueFactory<SlangDefinition, String>("definition"));
 
             view_table.setItems(slangData);
+
+            filter_def.textProperty().addListener((observable, oldValue, newValue) -> {
+                searchSlangByDef();
+            });
         } catch (IOException e) {
             // Handle the exception appropriately
         }
@@ -89,7 +111,6 @@ public class dashboardController {
         if (event.getSource() == view_all_btn) {
             view_all_form.setVisible(true);
             find_def_form.setVisible(false);
-
         }
         else if (event.getSource() == find_def_btn) {
             find_def_form.setVisible(true);
@@ -97,5 +118,21 @@ public class dashboardController {
         }
     }
 
+    @FXML
+    void searchSlangByDef() {
+        String searchTerm = filter_def.getText().toLowerCase().trim();
+        FilteredList<SlangDefinition> filteredData = new FilteredList<>(view_table.getItems());
+
+        filteredData.setPredicate(slangDefinition -> {
+            if (searchTerm.isEmpty()) {
+                return true;
+            }
+            return slangDefinition.getDefinition().toLowerCase().contains(searchTerm);
+        });
+
+        filter_slang_col.setCellValueFactory(new PropertyValueFactory<SlangDefinition, String>("slang"));
+        filter_def_col.setCellValueFactory(new PropertyValueFactory<SlangDefinition, String>("definition"));
+        filter_def_table.setItems(filteredData);
+    }
 
 }
